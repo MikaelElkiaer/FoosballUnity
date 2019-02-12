1b36ef6c71b9a1951246d7b620293ffbc7bccbf1
MikaelElkiaer <mikaelec@gmail.com>
2019-02-12 19:24:49 +0100

    Added Configuration controller.

---
+++ FoosballUnity/Controllers/ConfigurationController.cs
@@ -0,0 +1,31 @@
+﻿using FoosballUnity.Data;
+using FoosballUnity.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoosballUnity.Controllers
+{
+    [Route("configuration")]
+    public class ConfigurationController : Controller
+    {
+        private readonly FoosballContext context;
+
+        public ConfigurationController(FoosballContext context)
+        {
+            this.context = context;
+        }
+
+        [Route("")]
+        [HttpGet]
+        public async Task<ActionResult<ConfigurationItem[]>> Get()
+        {
+            var configurationItems = await context.Configurations.ToArrayAsync();
+
+            return configurationItems;
+        }
+    }
+}
